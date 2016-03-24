d2a90eeb9a57b171be8453e8c9994fe93d25b8aa
Pierre-edouard LIEB <plieb@e1r8p8.42.fr>
2016-03-24 10:50:49 +0100

    [Clean]

--- Example/Pods/Target Support Files/Pods-Recast.AI_Example/Pods-Recast.AI_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Recast_AI_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Recast_AI_Example
-@end
--- Example/Pods/Target Support Files/Recast.AI/Recast.AI-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Recast_AIVersionNumber;
-FOUNDATION_EXPORT const unsigned char Recast_AIVersionString[];
-
